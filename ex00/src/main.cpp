15e638c25265357534dec8b21d5747370ce88df3
ksura12 <private-mail@privat.de>
2023-04-03 15:16:57 +0200

    reading txt file

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ksura <ksura@student.42wolfsburg.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 11:09:08 by ksura             #+#    #+#             */
-/*   Updated: 2023/03/24 15:02:59 by ksura            ###   ########.fr       */
+/*   Updated: 2023/04/03 15:15:25 by ksura            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@
 #include <ctime>
 #include <cstdlib>
 
-int main(void)
+
+int read_csv_file(std::map<std::time_t, float> * datamap)
 {
     const char* filename = "src/data.csv";
-
     std::ifstream datafile(filename);
-
-    std::map<std::time_t, float> datamap;
+    // std::map<std::time_t, float> datamap;
+    // std::map<std::time_t, float>::iterator it;
 
     std::string format = "%Y-%m-%d";
     // char *strptime(const char *s, const char *format, struct tm *tm);
@@ -35,45 +35,130 @@ int main(void)
     if(datafile.is_open()){
         //read file line by line
         std::string line;
-        int linecount = 0;
+        int linecount = -1;
         while (std::getline(datafile, line)) {
             //splitting line at comma
+            linecount++;
             if (linecount == 0)
                 continue;
             std::stringstream ssline(line);
             std::string element;
             int i = 0;
-            struct tm tm;
+            struct tm tm_struct = {};
             float value = -1;
             while (std::getline(ssline, element, ','))
             {
                 if(i == 0)
                 {
-                    strptime(element.c_str(), "%Y-%m-%d", &tm);
+                    if(!strptime(element.c_str(), "%Y-%m-%d", &tm_struct))
+                    {
+                        std::cout << "Error: Failed to parse date string " << element << std::endl;
+                        break; 
+                    }
                 }
                 else
                     value = atof(element.c_str());
-
-                
-                std::cout << element << std::endl;
                 i++;
             }
             if (value != -1)
-                datamap.insert(std::pair<time_t, float>(mktime(&tm),value));
-            
-            //printing map
-            std::map<std::time_t, float>::iterator it;
-            std::cout << "Map Content:" <<std:: endl;
-            for (it = datamap.begin(); it != datamap.end(); it++)
-                std::cout << it->first << "---" << it->second << std::endl;
-            linecount++;
-            if (linecount == 3)
-                break;
+                datamap->insert(std::pair<time_t, float>(timegm(&tm_struct),value));
         }
+        // // Printing Map
+        // std::cout << "Map Content:" <<std:: endl;
+        // std::map<std::time_t, float>::iterator it;
+        // for (it = datamap->begin(); it != datamap->end(); it++)
+        //     std::cout << it->first << "---" << it->second << std::endl;
     } 
     else {
         std::cout << "ERROR: Could not read Data.csv" << std::endl;
-        return 1;
+        return (1);
     }
     datafile.close();
+    return  (0);
+}
+
+int read_txt_file()
+{
+    const char* filename = "input.txt";
+    std::ifstream txtfile(filename);
+    std::string format = "%Y-%m-%d";
+    
+    if(txtfile.is_open()){
+        //read file line by line
+        std::string line;
+        int linecount = -1;
+        while (std::getline(txtfile, line)) {
+            //splitting line at pipe
+            linecount++;
+            if (linecount == 0)
+                continue;
+            std::stringstream ssline(line);
+            std::string element;
+            int i = 0;
+            struct tm tm_struct = {};
+            float value = -1;
+            while (std::getline(ssline, element, '|'))
+            {
+                //cutting leading and following spaces and tabs
+                int leading = 0;
+                int following = element.length();
+                while(element[leading] == ' ' || element[leading] == '\t')
+                    leading++;
+                while(element[following] == ' ' || element[following] == '\t')
+                    leading--;
+                element = element.substr(leading, following - leading);
+                if(i == 0)
+                {
+                    if(!strptime(element.c_str(), "%Y-%m-%d", &tm_struct))
+                    {
+                        std::cout << "Error: Failed to parse date string " << element << std::endl;
+                        break; 
+                    }
+                }
+                else
+                {
+                    value = atof(element.c_str());
+                    if (value > 1000 || value < 0)
+                    {
+                        std::cout << "Error: Bitcoin Value not between 0-1000" << std::endl;
+                        value = -1;
+                        break;
+                    }
+                }   
+                i++;
+            }
+            if (value != -1)
+            //     datamap->insert(std::pair<time_t, float>(timegm(&tm_struct),value));
+                std::cout << timegm(&tm_struct) << " --- " << value << std::endl;
+        }
+        // // Printing Map
+        // std::cout << "Map Content:" <<std:: endl;
+        // std::map<std::time_t, float>::iterator it;
+        // for (it = datamap->begin(); it != datamap->end(); it++)
+        //     std::cout << it->first << "---" << it->second << std::endl;
+    } 
+    else {
+        std::cout << "ERROR: Could not read input.txt" << std::endl;
+        return (1);
+    }
+    txtfile.close();
+    return  (0);
+}
+
+
+int main(void)
+{
+    std::map<std::time_t, float> datamap;
+    
+    if (read_csv_file(&datamap))
+        return (1);
+    if (read_txt_file())
+        return (1);
+    
+    
+    // // Printing Map
+    // std::cout << "Map Content:" <<std:: endl;
+    // std::map<std::time_t, float>::iterator it;
+    // for (it = datamap.begin(); it != datamap.end(); it++)
+    //     std::cout << it->first << "---" << it->second << std::endl;
 }
